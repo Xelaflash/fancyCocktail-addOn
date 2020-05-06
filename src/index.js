4c5942fc2de7520f74017c7eaa9f0ac83c0b9523
Alexandre Gros <xelaflash@gmail.com>
2020-05-06 12:49:50 -0400

    TODO = redo functions with promise

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './assets/stylesheets/style.scss';
-import barrel from './assets/fonts/barrel.ttf';
 
+const loader = document.querySelector('.loader');
 const cocktailName = document.querySelector('.cocktail-name');
+const picture = document.querySelector('.cocktail-picture');
 
 function findById(id) {
   return function(singleCocktail) {
@@ -9,18 +10,45 @@ function findById(id) {
   };
 }
 
-// faire call API
-fetch('https://fancy-cocktails2.herokuapp.com/api/v1/cocktails')
-  .then(response => response.json())
-  .then(data => {
-    const resultsArray = data;
-    // recup ID des cocktails
-    const idArray = resultsArray.map(cocktail => cocktail.id);
-    // prendre une id random
-    const randomItem = idArray[Math.floor(Math.random() * idArray.length)];
-    // recuperate the good cocktail
-    const cocktailToDisplay = resultsArray.find(findById(randomItem));
-    console.log(cocktailToDisplay);
-    // display cocktail title
-    cocktailName.textContent = cocktailToDisplay.name;
-  });
+// call API for all cocktails to recuperate random cocktail ID
+async function fetchCocktails() {
+  const response = await fetch('https://fancy-cocktails2.herokuapp.com/api/v1/cocktails');
+  const data = await response.json();
+  // console.log(data);
+  return data;
+}
+
+// function getcocktailId() {
+//   const resultsArray = fetchCocktails();
+//   console.log(resultsArray);
+//   const idArray = resultsArray.map(element => element.id);
+//   const randomItem = idArray[Math.floor(Math.random() * idArray.length)];
+//   console.log(randomItem);
+//   return randomItem;
+// }
+// getcocktailId();
+
+// async function fetchCocktails() {
+//   const response = await fetch('https://fancy-cocktails2.herokuapp.com/api/v1/cocktails')
+//     .then(response => response.json())
+//     .then(data => {
+//       const resultsArray = data;
+//       // recup ID des cocktails
+//       const idArray = resultsArray.map(element => element.id);
+//       // prendre une id random
+//       const randomItem = idArray[Math.floor(Math.random() * idArray.length)];
+//       console.log(randomItem);
+//       return randomItem;
+//       // recuperate the good cocktail
+//       // const cocktailToDisplay = resultsArray.find(findById(randomItem));
+//       // console.log(cocktailToDisplay);
+//       // return cocktailToDisplay;
+//     });
+// }
+
+// re-call APi for specific cocktail
+// fetch(`https://fancy-cocktails2.herokuapp.com/api/v1/cocktails/33`)
+//   .then(response => response.json())
+//   .then(result => {
+//     console.log(result);
+//   });
