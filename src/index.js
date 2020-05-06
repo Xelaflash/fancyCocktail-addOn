a59daa9d88c27c6e1cf4cc5ffaf77724063370b4
Alexandre Gros <xelaflash@gmail.com>
2020-05-06 09:43:44 -0400

    fetching cocktail via APi

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './stylesheets/style.scss';
 
-console.log('hello from index.js');
+function findById(id) {
+  return function(singleCocktail) {
+    return singleCocktail.id === id;
+  };
+}
+
+// faire call API
+fetch('https://fancy-cocktails2.herokuapp.com/api/v1/cocktails')
+  .then(response => response.json())
+  .then(data => {
+    const resultsArray = data;
+    // recup ID des cocktails
+    const idArray = resultsArray.map(cocktail => cocktail.id);
+    // prendre une id random
+    const randomItem = idArray[Math.floor(Math.random() * idArray.length)];
+    // recuperate the good cocktail
+    const cocktailToDisplay = resultsArray.find(findById(randomItem));
+    console.log(cocktailToDisplay);
+  });
